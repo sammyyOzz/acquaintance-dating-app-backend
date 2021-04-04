0b4660800bd279fae7f2a1061addfdd797bf084d
Sammy <sammyoziegbe@gmail.com>
2021-04-04 13:08:14 +0100

    third deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const Cards = require('./dbCards.js')
 
 //App config
@@ -13,7 +14,9 @@ const connection_url = process.env.CONNECTION_URL
 
 // Middlewares
 app.use(cors())
-app.use(express.json())
+// app.use(express.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB config
 mongoose.connect(connection_url, {
