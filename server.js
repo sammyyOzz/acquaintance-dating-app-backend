71488abc58ab0adb2dc31b128e39df3010e7424e
Sammy <sammyoziegbe@gmail.com>
2021-04-04 12:35:45 +0100

    make it better

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/tinder/cards', (req, res) => {
         if (err) {
             res.status(500).send(err)
         } else {
-            res.status(201).send(data.reverse())
+            res.status(200).send(data)
         }
     })
 })
